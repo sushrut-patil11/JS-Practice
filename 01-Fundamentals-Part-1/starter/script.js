2f07c0dce82d499d9ef40e37ada207ebe01c0941
Sushrut <sushrutpatil5388@outlook.com>
2024-03-09 00:27:25 +0530

    new learning

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,33 @@
-let js = "amazing";
-if (js === "amazing") alert("JS is Fun!");
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10)
 
-40 + 8 + 23 - 10
-console.log(40 + 8 + 23 - 10)
+const country = "India";
+const continet = "Asia"
+let population = 1425775850;
+
+console.log(country);
+console.log(continet);
+console.log(population);
+
+
+let isIsland = false;
+const language = "hindi";
+// console.log(typeof isIsland);
+// console.log(typeof language);
+// console.log(typeof country);
+// const halfpopulation = population / 2;
+console.log(population / 2);
+
+const finpopulation = 600000;
+
+const islargerpopulation = population > finpopulation;
+
+console.log(islargerpopulation);
+
+const avgPopulation = 1400000;
+
+console.log(population > avgPopulation);
+
+const description = "Portugal is in Europe, and its 11 million people speak portuguese";
+console.log(description);
+
