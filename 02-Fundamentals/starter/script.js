fe151b263d048d04ae89ccaa2a96872bc8df062e
Sushrut <sushrutpatil5388@outlook.com>
2024-03-13 10:52:25 +0530

    Functions

---
+++ 02-Fundamentals/starter/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function describeCountry(country, population, capitalCity) {
+  console.log(
+    `${country} has ${population} people and its capital city is ${capitalCity}`
+  );
+}
+
+describeCountry("India", 10000000, " Delhi");
+describeCountry("USA", 200000, "New York");
+describeCountry("UK", 300000, "London");
