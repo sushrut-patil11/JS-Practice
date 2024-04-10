d47da9dcce3be01d865282de879112264b2de965
Sushrut <sushrutpatil5388@outlook.com>
2024-04-10 21:38:25 +0530

    Highscores

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,16 +1,23 @@
 'use strict';
 
-const SecretNumber = Math.trunc(Math.random() * 20) + 1;
+let SecretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = SecretNumber;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number provided!!!!!';
   } else if (guess === SecretNumber) {
     document.querySelector('.message').textContent = 'Correct Guess!!!!!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = SecretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } else if (guess < SecretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!!!';
@@ -28,4 +35,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You Lost the Game!!!';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  SecretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = '20';
+  document.querySelector('.message').textContent = 'Start Guessing...';
+  document.querySelector('body').style.backgroundColor = ' #222';
+  document.querySelector('.number').style.width = '14rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
 });
